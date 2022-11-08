fef51cde51d65e986f32e730b13880baeb6d3852
AbhishekIIITD <72123024+Abhi7012@users.noreply.github.com>
2022-11-08 15:00:08 +0530

    file structures created styles.js not used

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { CssBaseline } from "@mui/material";
+import { NavBar, Actors, Movies, MoviesInfo, Profiles } from "./index.js";
 
 const App = () => {
   return (
-    <div><h1>hey</h1></div>
-  )
-}
+    <div>
+      <CssBaseline />
+      <div className="root">
+        <NavBar />
+        <main className="content">
+        <div className="toolBar" />
+          <Routes>
+            <Route path="/" element={<Movies />} />
+            <Route path="/Actors:id" element={<Actors />} />
+            <Route path="/Movies:id" element={<Movies />} />
+            <Route path="/Profiles:id" element={<Profiles />} />
+            <Route path="/MoviesInfo:id" element={<MoviesInfo />} />
+          </Routes>
+        </main>
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
